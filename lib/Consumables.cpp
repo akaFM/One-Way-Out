630b766139066aaf9c4ed0c74b5a6b4c86da40d7
afCS100 <apham184@ucr.edu>
2024-03-14 23:29:47 -0700

    Optimized the constructors

--- lib/Consumables.cpp
+++ lib/Consumables.cpp
@@ -7,19 +7,24 @@ using namespace std;
 //consumableType is a string detaling what type it is poison, apple, health potion, etc. 
 
 //constructors
-    Consumables::Consumables() : changeHP(0), consumableType("")
-    {}
+    Consumables::Consumables() : changeHP(0)
+    {
+        Consumables c;
+        c.setName("");
+        c.setDescription("");
+    }
 
-    Consumables::Consumables(int cHP, string cT) : changeHP(cHP), consumableType(cT)
-    {}
+    Consumables::Consumables(string name, string description, int cHP) : changeHP(cHP)
+    {
+        Consumables c; 
+        c.setName(name);
+        c.setDescription(description);
+    }
 
 //setters
     void Consumables::setChangeHP(int hp) { changeHP = hp; }
 
-    void Consumables::setConsumableType(string cT) { consumableType = cT; }
-
 //getters 
     int Consumables::getChangeHP(){ return changeHP; }
 
-    string Consumables::getConsumableType() { return consumableType; }
 
